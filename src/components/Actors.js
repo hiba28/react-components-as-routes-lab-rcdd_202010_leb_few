60d2b356b1dd2fd61bf8d00d8e9509937e827f28
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2021-01-21 05:25:58 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,9 +5,17 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
-      <div>
-        <ul></ul>
-      </div>
+      {actors.map((actor, index) => (
+          <div key={index}>
+              <h3>Name: {actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {actor.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
